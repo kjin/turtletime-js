c93026138968d7ffeacd41e7ecd9bfbc5b6b598e
Kelvin Jin <kkj9@cornell.edu>
2016-07-14 17:30:03 -0400

    further improvements to ui editor

--- js/editor.js
+++ js/editor.js
@@ -1,15 +1,40 @@
 var load;
 var reset;
+var insert;
 var editor;
 
-function setup(file, storageKey, globalOptions) {
+function setup(file, storageKey, globalOptions, insertMaterial) {
+    if (!insertMaterial) {
+        insertMaterial = "";
+    }
     editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
     editor.getSession().setMode("ace/mode/json");
+    editor.commands.addCommand({
+        name: 'loadIntoTurtleTime',
+        bindKey: {win: 'Ctrl-B',  mac: 'Command-B'},
+        exec: function(editor) {
+            load();
+        },
+        readOnly: true
+    });
+    editor.commands.addCommand({
+        name: 'addCustom',
+        bindKey: {win: 'Ctrl-I',  mac: 'Command-I'},
+        exec: function(editor) {
+            insert();
+        },
+        readOnly: false
+    });
+    editor.setOptions({
+        scrollPastEnd: 0.8,
+        tabSize: 2
+    });
 
     var defData = "";
     if (localStorage.getItem(storageKey) != null) {
         editor.setValue(localStorage.getItem(storageKey));
+        editor.clearSelection();
         httpGet(file, function (resp) {
             defData = resp;
         });
@@ -17,6 +42,7 @@ function setup(file, storageKey, globalOptions) {
         httpGet(file, function (resp) {
             defData = resp;
             editor.setValue(defData);
+            editor.clearSelection();
         });
     }
 
@@ -28,7 +54,16 @@ function setup(file, storageKey, globalOptions) {
 
     reset = function() {
         editor.setValue(defData);
+        editor.clearSelection();
     };
+
+    insert = function() {
+        var currline = editor.getSelectionRange().start.row;
+        var wholelinetxt = editor.session.getLine(currline);
+        var leadingWhitespace = wholelinetxt.match(/^\s*/)[0];
+        var reformatted = insertMaterial.replace(/\n/g, '\n' + leadingWhitespace);
+        editor.insert(reformatted);
+    }
 
     function httpGet(url, callback) {
         var oReq = new XMLHttpRequest();
